3c9d1519a6b794137b9313bd8840d8e484801cd6
Kristina <kitty_kris@bk.ru>
2021-12-30 02:16:02 +0300

    create Controller

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,10 +2,6 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import web.model.User;
 import web.service.UserService;
 
 @Controller
