dcc835242e9d86e6e6afce144851bd9af60319b1
Kristina <kitty_kris@bk.ru>
2021-12-30 02:04:00 +0300

    fill methods in UserDaoImp

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -2,43 +2,41 @@ package web.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-    private SessionFactory sessionFactory;
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     public void addUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.save(user);
+        entityManager.persist(user);
     }
 
     @Override
     public User getUserById(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.find(User.class, id);
+        return entityManager.getReference(User.class, id);
     }
 
     @Override
     public void updateUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.update(user);
+        entityManager.refresh(user);
     }
 
     @Override
     public void deleteUser(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        session.delete(session.find(User.class, id));
+        entityManager.remove(entityManager.find(User.class, id));
     }
 
     @Override
     public List<User> userList() {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from User").list();
+        return entityManager.createQuery("SELECT users from User users").getResultList();
     }
 }
