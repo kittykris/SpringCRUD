e2354d8e46a7a8c2686a213c0e87dabda2c83d9b
Kristina <kitty_kris@bk.ru>
2021-12-30 02:05:26 +0300

    make all methods Transactional

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -10,29 +11,38 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-    @Autowired
-    UserDao userDao;
+    private UserDao userDao;
 
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
+    @Transactional
     @Override
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
+    @Transactional
     @Override
     public void deleteUser(long id) {
         userDao.deleteUser(id);
     }
 
+    @Transactional
     @Override
     public List<User> userList() {
         return userDao.userList();
