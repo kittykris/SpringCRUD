25430871f318c6d963bbe701784854f523a41cc1
Kristina <kitty_kris@bk.ru>
2022-01-01 22:14:17 +0300

    make Service not transactional

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,9 +2,8 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
-import web.model.User;
+import web.models.User;
 
 import java.util.List;
 
@@ -18,31 +17,26 @@ public class UserServiceImp implements UserService {
         this.userDao = userDao;
     }
 
-    @Transactional
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
-    @Transactional
     @Override
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
-    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
-    @Transactional
     @Override
     public void deleteUser(long id) {
         userDao.deleteUser(id);
     }
 
-    @Transactional
     @Override
     public List<User> userList() {
         return userDao.userList();
