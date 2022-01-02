95ddee5475d30710b94b6b75e7dbdc8d29927b08
Kristina <kitty_kris@bk.ru>
2022-01-02 04:30:59 +0300

    fill some methods in controller

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -17,9 +17,27 @@ public class UsersController {
         this.service = service;
     }
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public String usersList(Model model) {
-        model.addAttribute("users", service.userList());
+        model.addAttribute("userList", service.userList());
         return "users";
+    }
+
+    @GetMapping("/add")
+    public String addNewUser(Model model) {
+        model.addAttribute("newUser", new User());
+        return "addUser";
+    }
+
+    @PostMapping("/add")
+    public String addNewUser(@ModelAttribute User user, Model model) {
+        service.addUser(user);
+        model.addAttribute("newUser", user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/update/{userId}")
+    public String updateUser(Model model, @PathVariable("userId") String userId) {
+        return "redirect:/";
     }
 }
